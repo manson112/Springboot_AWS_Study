1e3110207f74b55e9dc868b376fb4a982362d103
manson112 <rlarhksdn777@naver.com>
2022-04-01 17:17:10 +0900

    initial commit

---
+++ src/main/java/testapp/SpringBootTestApp.java
@@ -0,0 +1,11 @@
+package testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestApp.class, args);
+    }
+}
