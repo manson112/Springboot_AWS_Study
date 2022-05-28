8b9a7e6feacff8dba017bc91010cf32594dd0ee8
manson112 <rlarhksdn777@naver.com>
2022-05-28 17:28:37 +0900

    nohup.out 파일로 output 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,4 +35,4 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
       -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
       -Dspring.profiles.active=real \
-      $REPOSITORY/$JAR_NAME 2>&1 &
+      $JAR_NAME > $REPOSITORY/nohub.out 2>&1 &
