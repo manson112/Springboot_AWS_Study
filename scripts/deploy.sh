a293d359c82a997c044f5db74ffce9e772c4bce1
manson112 <rlarhksdn777@naver.com>
2022-05-28 17:27:46 +0900

    줄바꿈 문제 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,7 +32,7 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar
-      -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties
-      -Dspring.profiles.active=real
+nohup java -jar \
+      -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+      -Dspring.profiles.active=real \
       $REPOSITORY/$JAR_NAME 2>&1 &
