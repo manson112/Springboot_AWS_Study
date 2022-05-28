4e0118d913b035d56e09bc2984ab7120019cb965
manson112 <rlarhksdn777@naver.com>
2022-05-28 18:47:17 +0900

    무중단 배포를 위한 profile controller 및 설정파일 추가

--- src/main/java/app/web/config/auth/SecurityConfig.java
+++ src/main/java/app/web/config/auth/SecurityConfig.java
@@ -17,7 +17,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .csrf().disable().headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션들을 disable
         .and()
             .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**") // 권한 관리 대상을 지정하는 옵션 URL, HTTP 메소드 별로 관리 가능
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile") // 권한 관리 대상을 지정하는 옵션 URL, HTTP 메소드 별로 관리 가능
                     .permitAll() // 위의 url들에 대해 전체 열람 권한
                 .antMatchers("/api/v1/**")
                     .hasRole(Role.USER.name()) // USER Role을 가진 사용자만 접근 가능
