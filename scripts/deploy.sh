c11b3bc6648137a7b3d5baf5c01af38dcb078704
manson112 <rlarhksdn777@naver.com>
2022-05-28 17:43:33 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -28,11 +28,14 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> JAR_NAME 에 실행권한 추가"
 
-chmod +x $JAR_NAME
+chmod +x "$JAR_NAME"
 
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-      -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties,classpath:/application-real.properties \
+      -Dspring.config.location=classpath:/application.properties, \
+      classpath:/application-real.properties, \
+      /home/ec2-user/app/application-oauth.properties, \
+      /home/ec2-user/app/application-real-db.properties \
       -Dspring.profiles.active=real \
-      $JAR_NAME > $REPOSITORY/nohub.out 2>&1 &
+      "$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
